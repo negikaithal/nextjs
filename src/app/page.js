95091b45ffce521b8fd26208a65ebbeea640c5f6
Your Name <negikaithal@gmail.com>
2024-07-15 17:41:40 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+// pages/signup.js
+"use client"
+import React from 'react';
+// import { Container, Box, AppBar, Toolbar, Typography, Button, TextField, MenuItem, Select, InputLabel, FormControl, Grid, Avatar } from '@mui/material';
+// import { makeStyles } from '@mui/styles';
+
+
+
+export default function SignUp() {
+  // const classes = useStyles();
+
+  return (
+    <>
+  <div>narender</div>
+     </>
+  );
+}
